6b282f4ed4e80a0d3b12e409263b54f13ceba798
arkajnag <arka.imps@gmail.com>
2020-08-07 16:06:15 +0200

    One Negative Test Scenario added in User Service for non-valid Userid to verify Error Message

--- src/test/java/io/CarPortal/TestCase/UserServiceTest.java
+++ src/test/java/io/CarPortal/TestCase/UserServiceTest.java
@@ -11,6 +11,8 @@ import org.testng.annotations.Test;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import io.CarPortal.Model.ErrorModel;
 import io.CarPortal.Model.User;
 import io.CarPortal.Model.UserDTO;
 import io.CarPortal.Utilities.CommonUtilities;
@@ -24,60 +26,87 @@ public class UserServiceTest extends BaseTest {
 	private final String userHostURL = getProps().getProperty("USER_HOST_URL");
 	private final String userServiceURLGetAllInfo = getProps().getProperty("USER_SERVICE_URL_GET_ALL_INFO");
 	private final String userServiceURLCreateNewUser = getProps().getProperty("USER_SERVICE_URL_CREATE_NEW_USER");
-	private final Map<String,String> headers=new HashMap<>();
-	
+	private final String userServiceURLGetByUserid = getProps().getProperty("USER_SERVICE_URL_GET_BY_USERID");
+	private final Map<String, String> headers = new HashMap<>();
+
 
 	@BeforeTest
 	public void setUp() {
 		mapper.registerModule(new JavaTimeModule());
 		headers.put("Content-Type", "application/json");
 	}
-	
+
 	@DataProvider
-	public Object[][] getUserInformation(){
+	public Object[][] getUserInformation() {
 		return new Object[][] {
-			{CommonUtilities.getRandomData.get(),"TestUser-"+CommonUtilities.getRandomData.get()},
-				{CommonUtilities.getRandomData.get(),"TestUser-"+CommonUtilities.getRandomData.get()}
-		};
+				{ CommonUtilities.getRandomData.get(), "TestUser-" + CommonUtilities.getRandomData.get() },
+				{ CommonUtilities.getRandomData.get(), "TestUser-" + CommonUtilities.getRandomData.get() } };
 	}
-	
+
 	@Test(priority = 1, description = "Verify New User Creation", dataProvider = "getUserInformation")
-	public void tcVerifyNewUserServiceTest(Integer userid,String username) {
+	public void tcVerifyNewUserServiceTest(Integer userid, String username) {
 		try {
-			logger.info(Thread.currentThread().getStackTrace()[2].getMethodName()+" has started its Execution");
-			String jsonPayloadString="";
-			User userRequestObj=new User(userid,username);
-				try {
-						jsonPayloadString=mapper.writeValueAsString(userRequestObj);
-					}catch(JsonProcessingException e) {
-						String exceptionMsg=CommonUtilities.stracktraceToString.apply(e);
-						logger.error("Failed in JSON Serializing. Exception Details:"+exceptionMsg);
-						Assert.fail("Failed in JSON Serializing. Exception Details:"+exceptionMsg);
+			logger.info(Thread.currentThread().getStackTrace()[2].getMethodName() + " has started its Execution");
+			String jsonPayloadString = "";
+			User userRequestObj = new User(userid, username);
+			try {
+				jsonPayloadString = mapper.writeValueAsString(userRequestObj);
+			} catch (JsonProcessingException e) {
+				String exceptionMsg = CommonUtilities.stracktraceToString.apply(e);
+				logger.error("Failed in JSON Serializing. Exception Details:" + exceptionMsg);
+				Assert.fail("Failed in JSON Serializing. Exception Details:" + exceptionMsg);
 			}
-			Response responseObj=WebServiceUtilities.postRequest(userHostURL+userServiceURLCreateNewUser, headers, jsonPayloadString);
+			Response responseObj = WebServiceUtilities.postRequest(userHostURL + userServiceURLCreateNewUser, headers,
+					jsonPayloadString);
 			Assert.assertTrue(CommonUtilities.verifyStatusCode.test(responseObj.getStatusCode()),
-			                    "Expected HttpResponse of CREATED or SUCCESS or OK is not received. Please review your request. Actual Response Code received:"+responseObj.getStatusCode());
-		}finally {
-			logger.info(Thread.currentThread().getStackTrace()[2].getMethodName()+" has end its Execution");
+					"Expected HttpResponse of CREATED or SUCCESS or OK is not received. Please review your request. Actual Response Code received:"
+							+ responseObj.getStatusCode());
+		} finally {
+			logger.info(Thread.currentThread().getStackTrace()[2].getMethodName() + " has end its Execution");
 		}
 	}
-	
-	@Test(priority = 2, description="Verify Search of All Users Available", dependsOnMethods = "tcVerifyNewUserServiceTest")
+
+	@Test(priority = 2, description = "Verify Search of All Users Available", dependsOnMethods = "tcVerifyNewUserServiceTest")
 	public void tcVerifySearchAllUsersInformationTest() {
 		try {
-			UserDTO userWrapperResponseObj=null;
-			logger.info(Thread.currentThread().getStackTrace()[2].getMethodName()+" has started its Execution");
-			Response responseObj=WebServiceUtilities.getRequest(userHostURL+userServiceURLGetAllInfo, headers);
+			UserDTO userWrapperResponseObj = null;
+			logger.info(Thread.currentThread().getStackTrace()[2].getMethodName() + " has started its Execution");
+			Response responseObj = WebServiceUtilities.getRequest(userHostURL + userServiceURLGetAllInfo, headers);
 			try {
-					userWrapperResponseObj=mapper.readValue(responseObj.asString(), UserDTO.class);
-			}catch(JsonProcessingException e) {
-				String exceptionMsg=CommonUtilities.stracktraceToString.apply(e);
-				logger.error("Failed in JSON De-Serializing. Exception Details:"+exceptionMsg);
-				Assert.fail("Failed in JSON De-Serializing. Exception Details:"+exceptionMsg);
+				userWrapperResponseObj = mapper.readValue(responseObj.asString(), UserDTO.class);
+			} catch (JsonProcessingException e) {
+				String exceptionMsg = CommonUtilities.stracktraceToString.apply(e);
+				logger.error("Failed in JSON De-Serializing. Exception Details:" + exceptionMsg);
+				Assert.fail("Failed in JSON De-Serializing. Exception Details:" + exceptionMsg);
 			}
-			Assert.assertTrue(CommonUtilities.greaterThan.apply(userWrapperResponseObj.getUsers().size(),0),"Please review your test data.");
-		}finally {
-			logger.info(Thread.currentThread().getStackTrace()[2].getMethodName()+" has end its Execution");
+			Assert.assertTrue(CommonUtilities.greaterThan.apply(userWrapperResponseObj.getUsers().size(), 0),
+					"Please review your test data.");
+		} finally {
+			logger.info(Thread.currentThread().getStackTrace()[2].getMethodName() + " has end its Execution");
+		}
+	}
+
+	@Test(priority = 3, description = "Negative Scenario: Verify Expected Error Message received when No Data found for specific Userid")
+	public void tcVerifyErrorMessageWhenNoDataForUseridFound() {
+		try {
+			int randomUserid = 98673;
+			ErrorModel errorResponseObj = null;
+			logger.info(Thread.currentThread().getStackTrace()[2].getMethodName() + " has started its Execution");
+			Response responseObj = WebServiceUtilities
+					.getRequest(userHostURL + userServiceURLGetByUserid + randomUserid, headers);
+			Assert.assertTrue(responseObj.getStatusCode() == 404,
+					"Expected Response Code not received for Data Not Found. Error Code received:"
+							+ responseObj.getStatusCode());
+			try {
+				errorResponseObj = mapper.readValue(responseObj.asString(), ErrorModel.class);
+			} catch (JsonProcessingException e) {
+				String exceptionMsg = CommonUtilities.stracktraceToString.apply(e);
+				logger.error("Failed in JSON De-Serializing. Exception Details:" + exceptionMsg);
+				Assert.fail("Failed in JSON De-Serializing. Exception Details:" + exceptionMsg);
+			}
+			Assert.assertEquals(errorResponseObj.getErrorMessage(), "No Data Present for User:"+randomUserid+". Please try with some other Userid", "Expected Error Message not received. Please review code.");
+		} finally {
+			logger.info(Thread.currentThread().getStackTrace()[2].getMethodName() + " has end its Execution");
 		}
 	}
 
